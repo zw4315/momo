6335899906d53d187ae07e2eaf302a9ee72ea23b
zw4315 <zhengyuwu_edward@outlook.com>
2025-08-22 15:32:17 +0800

    配置 vscode mac 模式下需要的配置文件

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 BUILD_DIR="$ROOT_DIR/build"
 
-cmake -S "$ROOT_DIR" -B "$BUILD_DIR"
+cmake -S "$ROOT_DIR" -B "$BUILD_DIR" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build "$BUILD_DIR" --parallel
 
